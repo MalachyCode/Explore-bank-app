63107222d114ab69acb3e836b992b2b775359e02
MalachyCode <nwaformalachy2@gmail.com>
2024-01-16 10:52:11 +0100

    Set up

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -10,7 +10,19 @@ function App() {
   const navigate = useNavigate();
 
   return (
-    <>
+    <div className='container'>
+      <div className='welcome-note'>
+        <h3>Welcome to Explore bank</h3>
+        <p>
+          At Explore bank, your financial success is our priority. We're
+          committed to providing you with secure, easy-to-use banking solutions
+          that fit your lifestyle. Let's make your dreams a reality, together!
+        </p>
+        <p>
+          Join us today and experience banking like never before. Welcome to a
+          world of endless possibilities. Welcome to Explore bank!
+        </p>
+      </div>
       <div className='card'>
         <Routes>
           <Route path='/login' element={<LoginPage />} />
@@ -21,7 +33,7 @@ function App() {
           count is {count}
         </button> */}
       </div>
-    </>
+    </div>
   );
 }
 
