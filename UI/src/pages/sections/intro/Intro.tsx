ce84f7dfb9d69b3478dc51f7d7ed78eb9152b6a5
MalachyCode <nwaformalachy2@gmail.com>
2024-01-16 23:27:15 +0100

    Building up home page

---
+++ UI/src/pages/sections/intro/Intro.tsx
@@ -0,0 +1,20 @@
+const Intro = () => {
+  return (
+    <div>
+      <div className='welcome-note intro' id='intro'>
+        <h3>Welcome to Explore bank</h3>
+        <p>
+          At Explore bank, your financial success is our priority. We're
+          committed to providing you with secure, easy-to-use banking solutions
+          that fit your lifestyle. Let's make your dreams a reality, together!
+        </p>
+        <p>
+          Join us today and experience banking like never before. Welcome to a
+          world of endless possibilities. Welcome to Explore bank!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
---
+++ UI/src/pages/sections/topbar/TopBar.tsx
@@ -0,0 +1,24 @@
+import './TopBar.scss';
+
+const TopBar = ({ menuOpen, setMenuOpen }) => {
+  return (
+    <div className={'topbar ' + (menuOpen && 'active')}>
+      <div className='wrapper'>
+        <div className='left'>
+          <a href='#intro' className='explore-logo'>
+            Explore.
+          </a>
+        </div>
+        <div className='right'>
+          <div className='hamburger' onClick={() => setMenuOpen(!menuOpen)}>
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
