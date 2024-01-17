89ff935069422f8244834b02355e941c29fc521d
MalachyCode <nwaformalachy2@gmail.com>
2024-01-17 12:35:18 +0100

    Dropdown menu added

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -4,6 +4,7 @@ import './App.scss';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import TopBar from './pages/sections/topbar/TopBar';
+import Menu from './pages/sections/menu/Menu';
 
 function App() {
   const navigate = useNavigate();
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className='app'>
       <TopBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className='container'>
         <Routes>
           <Route path='/login' element={<LoginPage />} />
--- UI/src/pages/sections/about/About.tsx
+++ UI/src/pages/sections/about/About.tsx
@@ -2,20 +2,22 @@ import './About.scss';
 const About = () => {
   return (
     <div className='about' id='about'>
-      <h3>About Us</h3>
-      <p>
-        At Explore bank, we believe in providing our customers with the best
-        possible banking experience. We are committed to offering a wide range
-        of financial products and services that cater to the needs of our
-        customers. Our team of experienced professionals is dedicated to
-        providing personalized service to each and every customer. We understand
-        that every customer has unique financial needs, and we strive to provide
-        customized solutions that meet those needs. We are proud to be a part of
-        the community and are committed to giving back. We support local
-        charities and organizations that make a positive impact on the lives of
-        people in our community. Thank you for choosing our bank. We look
-        forward to serving you.
-      </p>
+      <div className='about-message'>
+        <h3>About Us</h3>
+        <p>
+          At Explore bank, we believe in providing our customers with the best
+          possible banking experience. We are committed to offering a wide range
+          of financial products and services that cater to the needs of our
+          customers. Our team of experienced professionals is dedicated to
+          providing personalized service to each and every customer. We
+          understand that every customer has unique financial needs, and we
+          strive to provide customized solutions that meet those needs. We are
+          proud to be a part of the community and are committed to giving back.
+          We support local charities and organizations that make a positive
+          impact on the lives of people in our community. Thank you for choosing
+          our bank. We look forward to serving you.
+        </p>
+      </div>
     </div>
   );
 };
