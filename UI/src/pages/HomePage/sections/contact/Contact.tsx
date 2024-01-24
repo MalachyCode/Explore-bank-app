4bfb8357dfaf2ad1d39dfb84e92d1bbca42ba349
MalachyCode <nwaformalachy2@gmail.com>
2024-01-24 23:42:40 +0100

    Building the contact and footer sections of the home page

--- UI/src/pages/HomePage/sections/contact/Contact.tsx
+++ UI/src/pages/HomePage/sections/contact/Contact.tsx
@@ -1,9 +1,40 @@
 import './Contact.scss';
+import XIcon from '@mui/icons-material/X';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Contact = () => {
   return (
     <div className='contact' id='contact'>
-      Contact page
+      <div className='social-container'>
+        <h2>Connect on Social Media</h2>
+        <div className='icons-container'>
+          <XIcon fontSize='large' />
+          <FacebookIcon fontSize='large' />
+          <InstagramIcon fontSize='large' />
+          <LinkedInIcon fontSize='large' />
+        </div>
+      </div>
+      <div className='footer'>
+        <div className='footer-container'>
+          <div className='footer-column-1'>
+            <span className='line'></span>
+            <h4>Our Location</h4>
+            <span className='line'></span>
+          </div>
+          <div className='footer-column-2'>
+            <span className='line'></span>
+            <h4>Our Location</h4>
+            <span className='line'></span>
+          </div>
+          <div className='footer-column-3'>
+            <span className='line'></span>
+            <h4>Our Location</h4>
+            <span className='line'></span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
