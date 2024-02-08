b669d58e15f437510547b65f192c8df8bbcd144e
MalachyCode <nwaformalachy2@gmail.com>
2024-02-08 22:54:14 +0100

    Creating a bar chart to show income and expenses over the past 6 months

---
+++ UI/src/components/RenderIconsandTotals.tsx
@@ -0,0 +1,25 @@
+import { Icons } from '../types';
+
+export const RenderIcons = (props: Icons) => (
+  <div className='item'>
+    <img src={props.icon} alt='' className={props.iconClassName} />
+    <span className={props.spanClassName}>{props.label}</span>
+  </div>
+);
+
+export const RenderTotals = (props: {
+  className: string;
+  label: string;
+  amount: string;
+  percentage: string;
+}) => (
+  <div className={props.className}>
+    <div className='total-info'>
+      <h3>{props.label}</h3>
+      <strong>
+        <h2 className='amount'>{props.amount}</h2>
+      </strong>
+      <p className='savings-percentage'>{props.percentage}</p>
+    </div>
+  </div>
+);
