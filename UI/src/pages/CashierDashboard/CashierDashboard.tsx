ba999cfef30cc0b0ab9bc2165b263addfedbfbe5
MalachyCode <nwaformalachy2@gmail.com>
2024-02-12 17:27:05 +0100

    Building up and styling cahsier dashboard page

--- UI/src/pages/CashierDashboard/CashierDashboard.tsx
+++ UI/src/pages/CashierDashboard/CashierDashboard.tsx
@@ -1,7 +1,22 @@
 import './CahsierDashboard.scss';
 
 const CashierDashboard = () => (
-  <div className='cashier-dashboard'>Cashier dashbozrd</div>
+  <div className='cashier-dashboard'>
+    <div className='container'>
+      <div className='left'>
+        <div className='box'>Search for Client</div>
+        <div className='box'>Open Client Account</div>
+        <div className='box'>Credit Client Account</div>
+        <div className='box'>Generate Statement</div>
+      </div>
+      <div className='right'>
+        <div className='box'>All Clients</div>
+        <div className='box'>Money Transfer</div>
+        <div className='box'>Dedit Client Account</div>
+        <div className='box'>Update Account Info</div>
+      </div>
+    </div>
+  </div>
 );
 
 export default CashierDashboard;
