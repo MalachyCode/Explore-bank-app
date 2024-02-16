548eb6be548d85a2e94565b12413e86ffda2dd7e
MalachyCode <nwaformalachy2@gmail.com>
2024-02-16 21:19:07 +0100

    Implementing search functionality for staff dashboard to search through users in the database

--- UI/src/pages/StaffDashboard/sections/Search.tsx
+++ UI/src/pages/StaffDashboard/sections/Search.tsx
@@ -9,24 +9,56 @@ const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 
 const Search = () => {
   const [users, setUsers] = useState<Array<User>>([]);
+  const [showAll, setShowAll] = useState(true);
+  const [search, setSearch] = useState('');
   useEffect(() => {
     userService.getAll().then((users) => setUsers(users));
   }, []);
+
+  const usersToShow = showAll
+    ? users
+    : users.filter(
+        (user) =>
+          user.lastName.includes(search) || user.firstName.includes(search)
+      );
+  // console.log(users.map((user) => user.firstName.includes(search)));
+
   return (
     <div className='search'>
       <form onSubmit={handleSubmit}>
-        <input type='text' className='input' placeholder='Search for Client' />
+        <input
+          type='text'
+          className='input'
+          placeholder='Search for Client'
+          onChange={(e) => {
+            setShowAll(false);
+            setSearch(e.target.value);
+          }}
+        />
       </form>
       <div className='users'>
-        <ul>
-          {users.map((user) => (
-            <div>
-              <li key={user.id}>
-                {user.firstName} {user.lastName} {user.email}
-              </li>
-            </div>
-          ))}
-        </ul>
+        <table>
+          <thead>
+            <tr>
+              <th>Client Name</th>
+              <th>Email</th>
+              <th>Account Number</th>
+              <th className='phone'>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {usersToShow.map((user) => (
+              <tr key={user.id}>
+                <td>
+                  {user.firstName} {user.lastName}
+                </td>
+                <td>{user.email}</td>
+                <td>{user.number}</td>
+                <td className='phone'>{user.number}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
