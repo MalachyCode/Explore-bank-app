34a1d0d73f03b09a976931cd5ee48da22d2e7743
MalachyCode <nwaformalachy2@gmail.com>
2024-02-28 17:32:34 +0100

    Creating transaction control (debit/credit) form for staff dashboard

--- UI/src/pages/AccountPage/AccountPage.tsx
+++ UI/src/pages/AccountPage/AccountPage.tsx
@@ -4,11 +4,26 @@ import './AccountPage.scss';
 import accountsService from '../../services/accounts';
 import usersService from '../../services/users';
 import { useNavigate } from 'react-router-dom';
+import FormInput from '../../components/FormInput';
+import CloseIcon from '@mui/icons-material/Close';
+
+interface TransactionDetails {
+  amount: string;
+  description: string;
+}
 
 const AccountPage = (props: { user: User | null | undefined }) => {
   const navigate = useNavigate();
   const [user, setUser] = useState<User | null>();
   const [accounts, setAccounts] = useState<Array<Account>>([]);
+  const [transactionDetials, setTransactionDetials] =
+    useState<TransactionDetails>({
+      amount: '',
+      description: '',
+    });
+  const [transactionType, setTransactionType] = useState('');
+  const [accountNumber, setAccountNumber] = useState('');
+  const [transactionBoxOpen, setTransactionBoxOpen] = useState(false);
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedAppUser');
@@ -18,6 +33,28 @@ const AccountPage = (props: { user: User | null | undefined }) => {
     }
     accountsService.getAll().then((accounts) => setAccounts(accounts));
   }, []);
+
+  const formInputs = [
+    {
+      id: 'amount',
+      name: 'amount',
+      type: 'text',
+      placeholder: 'Amount',
+      errorMessage: 'Enter transfer amount',
+      label: 'Amount',
+      required: true,
+    },
+    {
+      id: 'description',
+      name: 'description',
+      type: 'text',
+      placeholder: 'Description',
+      errorMessage: `Enter transaction description longer than 5 characters`,
+      label: 'Description',
+      // pattern: '^[A-Za-z0-9]{5,500}$',
+      required: true,
+    },
+  ];
 
   const userAccounts = accounts.filter(
     (account) => account.owner === props.user?.id
@@ -38,8 +75,70 @@ const AccountPage = (props: { user: User | null | undefined }) => {
     navigate('/dashboard-staff/search/users');
   };
 
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTransactionDetials({
+      ...transactionDetials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className='account-page'>
+      <div className={'transaction-box ' + (transactionBoxOpen && 'open')}>
+        <CloseIcon
+          className='close'
+          onClick={() => setTransactionBoxOpen(false)}
+        />
+        <form
+          className='form'
+          onSubmit={(e) => {
+            console.log(transactionType);
+            console.log(accountNumber);
+            console.log(transactionDetials.amount);
+            console.log(transactionDetials.description);
+            console.log(accountNumber);
+
+            e.preventDefault();
+          }}
+        >
+          <label htmlFor='accountType'>Account Type</label>
+          <select
+            name='accountType'
+            id='accountType'
+            value={transactionType}
+            className='form-select'
+            onChange={(e) => setTransactionType(e.target.value)}
+          >
+            <option value='debit'>Debit</option>
+            <option value='credit'>Credit</option>
+          </select>
+          <label htmlFor='accountNumber'>Account Number</label>
+          <select
+            name='accountNumber'
+            id='accountNumber'
+            value={`${accountNumber}`}
+            className='form-select'
+            onChange={(e) => setAccountNumber(e.target.value)}
+          >
+            {userAccounts.map((account) => (
+              <option key={account.id} value={account.accountNumber}>
+                {account.accountNumber}
+              </option>
+            ))}
+          </select>
+          {formInputs.map((input) => (
+            <FormInput
+              key={input.id}
+              {...input}
+              value={transactionDetials[input.name as keyof TransactionDetails]}
+              onChange={onChange}
+            />
+          ))}
+          <button type='submit' className='btn'>
+            {transactionType === 'debit' ? 'Debit' : 'Credit'}
+          </button>
+        </form>
+      </div>
       <div className='container'>
         <div className='details-container'>
           <div className='name-container'>
@@ -62,8 +161,28 @@ const AccountPage = (props: { user: User | null | undefined }) => {
           </div>
         </div>
         <div className='buttons-container'>
-          {!user?.isAdmin && <button className='debit'>Debit Account</button>}
-          {!user?.isAdmin && <button className='credit'>Credit Account</button>}
+          {!user?.isAdmin && (
+            <button
+              className='debit'
+              onClick={() => {
+                setTransactionBoxOpen(true);
+                setTransactionType('debit');
+              }}
+            >
+              Debit Account
+            </button>
+          )}
+          {!user?.isAdmin && (
+            <button
+              className='credit'
+              onClick={() => {
+                setTransactionBoxOpen(true);
+                setTransactionType('credit');
+              }}
+            >
+              Credit Account
+            </button>
+          )}
           <button>Deactivate Account</button>
           <button
             className='delete'
