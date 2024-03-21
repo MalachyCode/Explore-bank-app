c9767e00bd8b9c210e9834c83619bae5728650c3
MalachyCode <nwaformalachy2@gmail.com>
2024-03-21 16:05:11 +0100

    Styling and arranging User Profile

--- UI/src/pages/UserProfile/UserProfile.tsx
+++ UI/src/pages/UserProfile/UserProfile.tsx
@@ -6,6 +6,8 @@ import './UserProfile.scss';
 const UserProfile = () => {
   const [user, setUser] = useState<User>();
   const [accounts, setAccounts] = useState<Array<Account>>([]);
+  const [enlargeProfilePicture, setEnlargeProfilePicture] = useState(false);
+
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedAppUser');
     if (loggedUserJSON) {
@@ -19,18 +21,43 @@ const UserProfile = () => {
 
   return (
     <div className='user-profile'>
-      <div className='profile-photo'></div>
-      User Profile
-      <div className='user-info'>
-        <div className='name'>
-          {user?.firstName} {user?.lastName}
+      <div className='container'>
+        <div className='top'>
+          <h3>User Profile</h3>
+          <div
+            className={'profile-photo ' + (enlargeProfilePicture && 'active')}
+            onClick={() => setEnlargeProfilePicture(!enlargeProfilePicture)}
+          >
+            {/* <img src='../../assets/pexels-aog-pixels-12698491.jpg' alt='' /> */}
+          </div>
+          <div className='user-name'>
+            {user?.firstName.toUpperCase()} {user?.lastName.toUpperCase()}
+          </div>
+          <div className='show-details'>Show Details</div>
         </div>
-        <div className='mail'>{user?.email}</div>
-        <div className='number'>{user?.number}</div>
-        <div className='account-numbers'>
-          {userAccounts.map((account) => (
-            <div key={account.id}>{account.accountNumber}</div>
-          ))}
+        <div className='body'>
+          <div className='user-info'>
+            <div className='name'>
+              <div className='headings'>Full Name</div>
+              {user?.firstName} {user?.lastName}
+            </div>
+            <div className='mail'>
+              <div className='headings'>Email</div>
+              {user?.email}
+            </div>
+            <div className='number'>
+              <div className='headings'>Phone</div>
+              {user?.number}
+            </div>
+            <div className='account-numbers'>
+              <div className='headings'>Account Number(s)</div>
+              {userAccounts.map((account) => (
+                <div key={account.id} className='account-number-box'>
+                  {account.accountNumber}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
