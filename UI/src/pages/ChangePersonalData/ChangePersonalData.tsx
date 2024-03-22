5113ecb26e8c0a02c93e258c683c819466cba8b8
MalachyCode <nwaformalachy2@gmail.com>
2024-03-22 13:58:08 +0100

    Creating new page to update user personal data

---
+++ UI/src/pages/ChangePersonalData/ChangePersonalData.tsx
@@ -0,0 +1,7 @@
+import './ChangePersonalData.scss';
+
+const ChangePersonalData = () => (
+  <div className='change-personal-data'>Change Personal Data</div>
+);
+
+export default ChangePersonalData;
