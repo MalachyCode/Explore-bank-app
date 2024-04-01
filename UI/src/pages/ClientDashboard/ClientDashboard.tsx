d408fd7d9aade110743ca5c7b65dd2ace798a91a
MalachyCode <nwaformalachy2@gmail.com>
2024-04-01 15:06:12 +0100

    Bug fix as the notification count algorithm was not written well so the notification alert was showing even though the user had no notifications

--- UI/src/pages/ClientDashboard/ClientDashboard.tsx
+++ UI/src/pages/ClientDashboard/ClientDashboard.tsx
@@ -29,13 +29,13 @@ const ClientDashboard = (props: { handleLogout: () => void }) => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON);
       notificationsService.getAll().then((allNotifications) => {
-        console.log(allNotifications);
-        console.log(user);
+        // console.log(allNotifications);
+        // console.log(user);
 
         setNotificationCount(
           allNotifications.find(
             (notification: Notification) => notification.owner === user.id
-          )?.newNotifications.length
+          )?.newNotifications.length || 0
         );
         setNotifications(allNotifications);
       });
