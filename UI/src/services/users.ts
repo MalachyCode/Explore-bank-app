03764a19083cdd2b9968c40ae8db7c791973f7c7
MalachyCode <nwaformalachy2@gmail.com>
2024-04-12 22:33:45 +0100

    Connecting user service to the backend

--- UI/src/services/users.ts
+++ UI/src/services/users.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { NewUser, User } from '../types';
-const baseUrl = 'http://localhost:3001/users';
+const baseUrl = 'http://localhost:3001/api/users';
+// const baseUrl = 'http://localhost:3001/users';
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
