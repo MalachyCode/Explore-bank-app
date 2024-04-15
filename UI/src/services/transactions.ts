85f52cb399e40f416a04384600bf0b6ffdff2f27
MalachyCode <nwaformalachy2@gmail.com>
2024-04-15 22:20:58 +0100

    Connecting transactions service from the frontend to the backend

--- UI/src/services/transactions.ts
+++ UI/src/services/transactions.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { NewTransaction } from '../types';
-const baseUrl = 'http://localhost:3001/transactions';
+const baseUrl = 'http://localhost:3001/api/transactions';
+// const baseUrl = 'http://localhost:3001/transactions';
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
