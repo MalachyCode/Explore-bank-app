d6359bb7193c9b89d0346e8dec74b9d4405a98d5
MalachyCode <nwaformalachy2@gmail.com>
2024-04-16 16:02:02 +0100

    Notifications service (end points and connections)

---
+++ Backend/controllers/notifications.ts
@@ -0,0 +1,82 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const notificationsRouter = require('express').Router()
+import { NextFunction, Request, Response } from 'express'
+import Notification from '../models/notification'
+
+notificationsRouter.get('/', (_req: Request, res: Response) => {
+  Notification.find({}).then((notifications) => {
+    res.json(notifications)
+  })
+})
+
+notificationsRouter.get(
+  '/:id',
+  (req: Request, res: Response, next: NextFunction) => {
+    Notification.findById(req.params.id)
+      .then((notification) => {
+        if (notification) {
+          res.json(notification)
+        } else {
+          res.status(404).end()
+        }
+      })
+      .catch((error) => next(error))
+  }
+)
+
+notificationsRouter.post(
+  '/',
+  (req: Request, res: Response, next: NextFunction) => {
+    const body = req.body
+
+    const notification = new Notification({
+      owner: body.owner,
+      oldNotifications: body.oldNotifications,
+      newNotifications: body.newNotifications,
+    })
+
+    notification
+      .save()
+      .then((savedNotification) => {
+        res.json(savedNotification)
+      })
+      .catch((error) => next(error))
+  }
+)
+
+notificationsRouter.delete(
+  '/:id',
+  (req: Request, res: Response, next: NextFunction) => {
+    Notification.findByIdAndDelete(req.params.id)
+      .then(() => {
+        res.status(204).end()
+      })
+      .catch((error) => next(error))
+  }
+)
+
+notificationsRouter.put(
+  '/:id',
+  (req: Request, res: Response, next: NextFunction) => {
+    const body = req.body
+
+    const notification = {
+      owner: body.owner,
+      oldNotifications: body.oldNotifications,
+      newNotifications: body.newNotifications,
+    }
+
+    Notification.findByIdAndUpdate(req.params.id, notification, {
+      new: true,
+      runValidators: true,
+      context: 'query',
+    })
+      .then((updatedNotification) => {
+        res.json(updatedNotification)
+      })
+      .catch((error) => next(error))
+  }
+)
+
+// module.exports = usersRouter
+export default notificationsRouter
