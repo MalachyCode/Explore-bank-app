47a7542656eeff96cd7f2c44cd9d9a8b50569e51
MalachyCode <nwaformalachy2@gmail.com>
2024-04-30 16:13:01 +0100

    Configuring and using Multer for profile picture uploads

--- Api/src/routes/users.ts
+++ Api/src/routes/users.ts
@@ -4,6 +4,88 @@ const bcrypt = require('bcrypt');
 const usersRouter = require('express').Router();
 import { NextFunction, Request, Response } from 'express';
 import User from '../models/user';
+// import multer from 'multer';
+import multer, { FileFilterCallback } from 'multer';
+type DestinationCallback = (error: Error | null, destination: string) => void;
+type FileNameCallback = (error: Error | null, filename: string) => void;
+
+const storage = multer.diskStorage({
+  destination: function (
+    _req: Request,
+    _file: Express.Multer.File,
+    cb: DestinationCallback
+  ) {
+    cb(null, './profilePictures/');
+  },
+  filename: function (
+    _req: Request,
+    file: Express.Multer.File,
+    cb: FileNameCallback
+  ) {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+const fileFilter = (
+  _req: Request,
+  file: Express.Multer.File,
+  cb: FileFilterCallback
+) => {
+  // accept a file
+  if (
+    file.mimetype === 'image/jpeg' ||
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg'
+  ) {
+    cb(null, true);
+  } else {
+    // reject a file
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+  fileFilter: fileFilter,
+});
+
+usersRouter.put(
+  '/upload/:id',
+  upload.single('profilePicture'),
+  (req: Request, res: Response, next: NextFunction) => {
+    const body = req.body;
+
+    // console.log('file', req.file);
+    // console.log('body', req.body);
+    // res.json(req.file);
+    const user = {
+      email: body.email,
+      firstName: body.firstName,
+      lastName: body.lastName,
+      middleName: body.middleName,
+      password: body.password,
+      type: body.type,
+      isAdmin: body.isAdmin,
+      number: body.number,
+      dob: body.dob,
+      transferPin: body.transferPin,
+      profilePicture: req.file?.path,
+    };
+
+    console.log(user);
+
+    User.findByIdAndUpdate(req.params.id, user, {
+      new: true,
+      runValidators: true,
+      context: 'query',
+    })
+      .then((updatedUser) => {
+        res.json(updatedUser);
+      })
+      .catch((error) => next(error));
+  }
+);
 
 usersRouter.get('/', (_req: Request, res: Response) => {
   User.find({}).then((users) => {
@@ -69,9 +151,6 @@ usersRouter.put(
   async (req: Request, res: Response, next: NextFunction) => {
     const body = req.body;
 
-    const saltRounds = 10;
-    const transferPinHash = await bcrypt.hash(body.transferPin, saltRounds);
-
     const user = {
       email: body.email,
       firstName: body.firstName,
@@ -82,7 +161,7 @@ usersRouter.put(
       isAdmin: body.isAdmin,
       number: body.number,
       dob: body.dob,
-      transferPin: transferPinHash,
+      transferPin: body.transferPin,
     };
 
     User.findByIdAndUpdate(req.params.id, user, {
