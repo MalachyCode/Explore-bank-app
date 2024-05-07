fe55b893877dcd77e5e84efae76b905c84496569
MalachyCode <nwaformalachy2@gmail.com>
2024-05-08 00:09:27 +0100

    Connecting to node backend.

--- UI/src/services/incomeExpense.ts
+++ UI/src/services/incomeExpense.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-
-const baseUrl = 'http://localhost:3002/bar-chart-info';
+const baseUrl = 'http://localhost:3001/api/bar-chart-info';
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
