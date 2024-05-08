5476034a53c7a1b2e2463c9bf67abc0f7b95ce8f
MalachyCode <nwaformalachy2@gmail.com>
2024-05-08 22:21:50 +0100

    Added end points create, delete, update and find user barchart info in the incomeExpense.ts service file in the frontend

--- UI/src/services/incomeExpense.ts
+++ UI/src/services/incomeExpense.ts
@@ -1,11 +1,42 @@
 import axios from 'axios';
+import { BarChartInfo, FindUserAccountsType, NewBarChartInfo } from '../types';
 const baseUrl = 'http://localhost:3001/api/bar-chart-info';
+// const baseUrl = 'http://localhost:3002/bar-chart-info';
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
   return response.data;
 };
 
+const create = async (NewBarChartInfo: NewBarChartInfo) => {
+  const response = await axios.post(baseUrl, NewBarChartInfo);
+  return response.data;
+};
+
+const updateBarChartInfo = async (
+  id: string,
+  updatedBarChartInfo: BarChartInfo
+) => {
+  const response = await axios.put(`${baseUrl}/${id}`, updatedBarChartInfo);
+
+  return response.data;
+};
+
+const findUserBarChartInfo = async (owner: FindUserAccountsType) => {
+  const response = await axios.post(`${baseUrl}/user-bar-chart-info`, owner);
+  return response.data;
+};
+
+const deleteBarChartInfo = async (id: string) => {
+  const response = await axios.delete(`${baseUrl}/${id}`);
+
+  return response.data;
+};
+
 export default {
+  create,
+  deleteBarChartInfo,
+  findUserBarChartInfo,
   getAll,
+  updateBarChartInfo,
 };
