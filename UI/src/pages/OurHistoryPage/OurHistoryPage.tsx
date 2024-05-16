a388ab82b4986b530957d4d7299ca6e3980915fb
MalachyCode <nwaformalachy2@gmail.com>
2024-05-16 21:12:37 +0100

    New our history page created with a brief history write up.

---
+++ UI/src/pages/OurHistoryPage/OurHistoryPage.tsx
@@ -0,0 +1,27 @@
+import './OurHistoryPage.scss';
+
+const OurHistoryPage = () => (
+  <div className='our-history'>
+    <div className='container'>
+      Explore Bank, a financial institution based in Nigeria, was established in
+      2024. The bank was founded by a group of visionary entrepreneurs who saw
+      the need for a customer-centric bank that would prioritize the financial
+      needs of its clients. The founders, led by Mr. Nwafor Malachy, aimed to
+      revolutionize the banking sector in Nigeria. They believed in the
+      potential of the Nigerian economy and wanted to contribute to its growth
+      by providing innovative banking solutions. In its first year, Explore Bank
+      obtained its banking license from the Central Bank of Nigeria (CBN). The
+      bank offered a range of services including savings and current accounts,
+      loans, and investment products. Explore Bank quickly gained a reputation
+      for excellent customer service and innovative digital banking solutions.
+      The bank also launched its mobile banking app, which was well-received by
+      customers for its user-friendly interface and robust features. Despite the
+      economic challenges posed by the COVID-19 pandemic, Explore Bank continues
+      to thrive and expand. Today, Explore Bank is recognized as one of the
+      leading banks in Nigeria, committed to delivering excellent financial
+      services and contributing to the economic development of the country.
+    </div>
+  </div>
+);
+
+export default OurHistoryPage;
