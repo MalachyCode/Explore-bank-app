c4ad7f7b7848fd41c31a7ee1bd33af66d9db95db
MalachyCode <nwaformalachy2@gmail.com>
2024-06-05 14:35:02 +0100

    Changing from react-dropdown library and using regular form select attribut.

--- UI/src/pages/HomePage/sections/currencyConverter/CurrencyConverterPage.tsx
+++ UI/src/pages/HomePage/sections/currencyConverter/CurrencyConverterPage.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import Axios from 'axios';
-import Dropdown from 'react-dropdown';
 import { HiSwitchHorizontal } from 'react-icons/hi';
 import 'react-dropdown/style.css';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import './CurrencyConverterPage.scss';
 import { useNavigate } from 'react-router-dom';
 import CurrencyInput from 'react-currency-input-field';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const CurrencyConverterPage = () => {
   const navigate = useNavigate();
@@ -68,6 +68,7 @@ const CurrencyConverterPage = () => {
               onChange={(e) => setInput(Number(e.target.value))}
             /> */}
             <CurrencyInput
+              className='currency-input'
               value={input ? input : 0}
               onValueChange={(amount) => setInput(Number(amount ? amount : 0))}
               intlConfig={{ locale: 'en-US', currency: from }}
@@ -78,14 +79,24 @@ const CurrencyConverterPage = () => {
           <div className='right-container'>
             <div className='middle'>
               <h3>From</h3>
-              <Dropdown
-                options={options}
-                onChange={(e) => {
-                  setFrom(e.value);
-                }}
-                value={from}
-                placeholder='From'
-              />
+              <div className='select-container'>
+                <select
+                  className='select-dropdown'
+                  name='from-currency'
+                  id=''
+                  value={from}
+                  onChange={(e) => {
+                    setFrom(e.target.value);
+                  }}
+                >
+                  {options.map((option) => (
+                    <option value={option}>{option}</option>
+                  ))}
+                </select>
+                <div className='icon-container'>
+                  <ArrowDropDownIcon className='icon' />
+                </div>
+              </div>
             </div>
             <div className='switch'>
               <HiSwitchHorizontal
@@ -97,14 +108,24 @@ const CurrencyConverterPage = () => {
             </div>
             <div className='right'>
               <h3>To</h3>
-              <Dropdown
-                options={options}
-                onChange={(e) => {
-                  setTo(e.value);
-                }}
-                value={to}
-                placeholder='To'
-              />
+              <div className='select-container'>
+                <select
+                  className='select-dropdown'
+                  name='to-currency'
+                  id=''
+                  value={to}
+                  onChange={(e) => {
+                    setTo(e.target.value);
+                  }}
+                >
+                  {options.map((option) => (
+                    <option value={option}>{option}</option>
+                  ))}
+                </select>
+                <div className='icon-container'>
+                  <ArrowDropDownIcon className='icon' />
+                </div>
+              </div>
             </div>
           </div>
         </div>
