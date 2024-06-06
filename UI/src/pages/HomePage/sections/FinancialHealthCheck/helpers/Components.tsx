5a95f0bef313f3ee859f68985b974845ca6a420b
MalachyCode <nwaformalachy2@gmail.com>
2024-06-06 22:23:21 +0100

    A new module created to contain all the components used to render the health check bars to the screen.

---
+++ UI/src/pages/HomePage/sections/FinancialHealthCheck/helpers/Components.tsx
@@ -0,0 +1,35 @@
+export const CriticalBar = () => (
+  <div>
+    <span className='critical'></span>
+  </div>
+);
+export const OkayBar = () => (
+  <div>
+    <span className='okay'></span>
+    <span className='critical'></span>
+  </div>
+);
+export const GoodBar = () => (
+  <div>
+    <span className='good'></span>
+    <span className='okay'></span>
+    <span className='critical'></span>
+  </div>
+);
+export const GreatBar = () => (
+  <div>
+    <span className='great'></span>
+    <span className='good'></span>
+    <span className='okay'></span>
+    <span className='critical'></span>
+  </div>
+);
+export const ExcellentBar = () => (
+  <div>
+    <span className='excellent'></span>
+    <span className='great'></span>
+    <span className='good'></span>
+    <span className='okay'></span>
+    <span className='critical'></span>
+  </div>
+);
