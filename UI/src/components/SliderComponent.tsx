a0247eb9a2f63350c2d0958c71d0461f23d2c84a
MalachyCode <nwaformalachy2@gmail.com>
2024-06-12 23:24:22 +0100

    Creating another type of numbers slider using material ui slider component to be used also for displaying data in the retirement planner page.

---
+++ UI/src/components/SliderComponent.tsx
@@ -0,0 +1,35 @@
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Slider from '@mui/material/Slider';
+import { useState } from 'react';
+import Typography from '@mui/material/Typography';
+
+interface TitleType {
+  title: string;
+}
+
+const SliderComponent = (props: TitleType) => {
+  const [value, setValue] = useState<number>(30);
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setValue(newValue as number);
+  };
+
+  return (
+    <Box sx={{ width: 150 }}>
+      <Typography id='input-slider' gutterBottom>
+        {props.title}: {value}
+      </Typography>
+      <Stack spacing={2} direction='row' sx={{ mb: 1 }} alignItems='center'>
+        <Slider
+          aria-label={props.title}
+          size='small'
+          value={value}
+          onChange={handleChange}
+        />
+      </Stack>
+    </Box>
+  );
+};
+
+export default SliderComponent;
