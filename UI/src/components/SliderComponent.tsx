aca4b2bc53e68c36c58a95860d470f90a7e58191
MalachyCode <nwaformalachy2@gmail.com>
2024-06-14 22:05:48 +0100

    On change event handler now passed as a prop to the component as well as formatting the titile of the slider to add currency or percentage symbol depending on the title.

--- UI/src/components/SliderComponent.tsx
+++ UI/src/components/SliderComponent.tsx
@@ -1,31 +1,33 @@
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Slider from '@mui/material/Slider';
-import { useState } from 'react';
 import Typography from '@mui/material/Typography';
 
 interface TitleType {
   title: string;
+  handleChange: (event: Event, newValue: number | number[]) => void;
+  value: number;
 }
 
 const SliderComponent = (props: TitleType) => {
-  const [value, setValue] = useState<number>(30);
-
-  const handleChange = (event: Event, newValue: number | number[]) => {
-    setValue(newValue as number);
-  };
-
   return (
     <Box sx={{ width: 150 }}>
-      <Typography id='input-slider' gutterBottom fontSize={13}>
-        {props.title}: {value}
+      <Typography id='input-slider' gutterBottom fontSize={12}>
+        {props.title}:{' '}
+        {props.title === 'Salary'
+          ? 'NGN'
+          : props.title !== 'Age' && props.title !== 'Retirement Age'
+          ? '%'
+          : ''}
+        {props.value}
+        {props.title === 'Salary' ? '0000' : ''}
       </Typography>
       <Stack spacing={2} direction='row' sx={{ mb: 1 }} alignItems='center'>
         <Slider
           aria-label={props.title}
           size='small'
-          value={value}
-          onChange={handleChange}
+          value={props.value}
+          onChange={props.handleChange}
         />
       </Stack>
     </Box>
