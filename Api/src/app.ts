c5af2f968ff6d7a4c0b193dbb03bfcc4a2a14df1
MalachyCode <nwaformalachy2@gmail.com>
2024-06-17 12:59:02 +0100

    Using static middleware to render production build of the frontend.

--- Api/src/app.ts
+++ Api/src/app.ts
@@ -28,7 +28,7 @@ if (config.MONGODB_URI) {
 }
 
 app.use(cors());
-// app.use(express.static('build'));
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
